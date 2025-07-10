f593993159d9ec640f274224dcd6332ea7d7b800
Dilumuene Kiaku <chimiefriends@gmail.com>
2025-07-10 16:57:25 +0200

    Update lierFunction.go

--- lierFunction.go
+++ lierFunction.go
@@ -2,19 +2,19 @@ package main
 
 import "fmt"
 
-type Example struct {
-	a string
-	b int
-	c bool
-}
-
 func main() {
 	myContact := newContact("Winslet", 12, map[string]string{"fixe": "01.54.14.25",
 		"Portable": "06.56.24.15.12"})
 
+	fmt.Println(myContact.displayContact())
+
 	myNewContact := newContact("Otniel", 30, map[string]string{"fixe": "01.52.41.25",
 		"Portable": "07.56.24.57.62"})
 
-	fmt.Println(myContact)
 	fmt.Println(myNewContact.displayContact())
+
+	myNewNewContact := newContact("Magali", 28, map[string]string{"fixe": "01.22.46.55",
+		"Portable": "06.55.24.59.63"})
+
+	fmt.Println(myNewNewContact.displayContact())
 }
